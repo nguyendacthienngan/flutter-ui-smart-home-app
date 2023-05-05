1fc692a93bc67f474bf6fa26c57de3ea14c223d9
nguyendacthienngan <nguyendacthienngan@gmail.com>
2023-05-05 15:20:21 +0700

    Add Firebase Cloud Messaging (Push notification feature)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_home/src/injector.dart';
 import 'package:flutter_smart_home/src/ui/pages/auth_page.dart';
+import 'package:overlay_support/overlay_support.dart';
 
 import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,13 +36,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Smart Home App',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
+    return OverlaySupport.global(
+      child: MaterialApp(
+        title: 'Flutter Smart Home App',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: const AuthPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const AuthPage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/src/ui/pages/home_page.dart
+++ lib/src/ui/pages/home_page.dart
@@ -1,7 +1,15 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_smart_home/src/data/models/push_notification_model.dart';
+import 'package:flutter_smart_home/src/ui/notification_badge.dart';
 import 'package:flutter_smart_home/src/ui/pages/temperature_page.dart';
+import 'package:overlay_support/overlay_support.dart';
+
+Future _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
+}
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +19,68 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late int _totalNotifications;
+  late final FirebaseMessaging _messaging;
+  PushNotification? _notificationInfo;
+
+  void requestAndRegisterNotification() async {
+    // Instantiate Firebase Messaging
+    _messaging = FirebaseMessaging.instance;
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    // On IOS, this helps to take the user permissions (Most important step)
+    NotificationSettings settings = await _messaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+      // Try to get the token, each device will have 1 unique token (on each time installation)
+      // TODO: Save to the Database
+      String? token = await _messaging.getToken();
+      print("The token is " + token!);
+
+      // For handling the received notifications
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        // Parse the message received
+        PushNotification notification = PushNotification(
+          title: message.notification?.title,
+          body: message.notification?.body
+        );
+
+        setState(() {
+          _notificationInfo = notification;
+          _totalNotifications++;
+        });
+
+        if (_notificationInfo != null) {
+          // For displaying the notification as an overlay
+          showSimpleNotification(
+            Text(_notificationInfo!.title!),
+            leading: NotificationBadge(totalNotifications: _totalNotifications),
+            subtitle: Text(_notificationInfo!.body!),
+            background: Colors.cyan.shade700,
+            duration: const Duration(seconds: 2),
+          );
+        }
+      });
+    } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+      print('User granted provisional permission');
+    } else {
+      print('User declined or has not accepted permission');
+    }
+  }
+
+  @override
+  void initState() {
+    _totalNotifications = 0;
+    requestAndRegisterNotification();
+    super.initState();
+  }
+
   // Sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
